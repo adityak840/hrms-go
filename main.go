ad342c1266eb5b522cdf5d86cbf4199681c9bcb1
adityak840 <khandelwal.aditya840@gmail.com>
2023-08-18 14:47:40 +0530

    fix: import

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"time"
 
-	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
